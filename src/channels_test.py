d98069737e54aa15b9625b1d52a01d0812249214
Mufeed <z5120474@student.unsw.edu.au>
2020-03-05 14:01:44 +1100

    Fixed a typo

--- src/channels_test.py
+++ src/channels_test.py
@@ -2,7 +2,7 @@ import channels
 import pytest
 from error import InputError
 
-def test_channel_create():
+def test_channels_create():
     assert channels.channels_create(12345, 'Bob', True) == 1
     assert channels.channels_create(12345, 'Bob', False) == 1
     assert channels.channels_create(12345, '', False) == 0
@@ -10,7 +10,7 @@ def test_channel_create():
     assert channels.channels_create(, '', False) == 0
     assert channels.channels_create(, '', ) == 0
 
-def test_channel_except():
+def test_channels_except():
     with pytest.raises(InputError) as e:
         #assert channels.channels_create() HOW DO I TEST THE INPUT VARIABLE??
         assert channels.channels_create(12345,'yeeeeeeeeeeeeeeeeeeeeet',True)
