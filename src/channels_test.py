ed181fd31b25f65b59da6f4040451ec04795276f
Mufeed <z5120474@student.unsw.edu.au>
2020-03-06 16:29:21 +1100

    Mufeeds tests

--- src/channels_test.py
+++ src/channels_test.py
@@ -3,14 +3,17 @@ import pytest
 from error import InputError
 
 def test_channels_create():
+    assert channels.channels_create(12345, 'Ben', False) == 1
     assert channels.channels_create(12345, 'Bob', True) == 1
-    assert channels.channels_create(12345, 'Bob', False) == 1
-    assert channels.channels_create(12345, '', False) == 0
-    assert channels.channels_create(, 'Bob', False) == 0
-    assert channels.channels_create(, '', False) == 0
-    assert channels.channels_create(, '', ) == 0
+    list_of_channels = channels_listall(12345)
+    
+    #check if it has the names in it
+    flag =0
+    if Bob in list_of_channels[name]
+        flag =1
+    assert flag == 1
+
 
 def test_channels_except():
     with pytest.raises(InputError) as e:
-        #assert channels.channels_create() HOW DO I TEST THE INPUT VARIABLE??
-        assert channels.channels_create(12345,'yeeeeeeeeeeeeeeeeeeeeet',True)
+        channels.channels_create(12345,'yeeeeeeeeeeeeeeeeeeeeet',True)
--- src/user_test.py
+++ src/user_test.py
@@ -3,7 +3,7 @@ import pytest
 from error import InputError
 
 def test_channels_create():
-    assert user.user_profile(12345, 1) ==  {'email': 'cs1531@cse.unsw.edu.au','name_first': 'Hayden','name_last': 'Jacobs', 	'handle_str': 'hjacobs')
+    assert user.user_profile(12345, 1) ==  {'u_id': 1,'email': 'cs1531@cse.unsw.edu.au','name_first': 'Hayden','name_last': 'Jacobs', 	'handle_str': 'hjacobs'}
    
 def test_channels_except():
     with pytest.raises(InputError) as e:
