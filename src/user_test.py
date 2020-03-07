332e45a59a7f7c388b9e3a7a04febae351182ab7
Mufeed <z5120474@student.unsw.edu.au>
2020-03-07 15:42:20 +1100

    Fixed a mistake

--- src/user_test.py
+++ src/user_test.py
@@ -2,10 +2,10 @@ import user
 import pytest
 from error import InputError
 
-def test_channels_create():
+def test_user_profile():
     assert user.user_profile(12345, 1) ==  {'u_id': 1,'email': 'cs1531@cse.unsw.edu.au','name_first': 'Hayden','name_last': 'Jacobs', 	'handle_str': 'hjacobs'}
    
-def test_channels_except():
+def test_user_profile():
     with pytest.raises(InputError) as e:
         #assert channels.channels_create() HOW DO I TEST THE INPUT VARIABLE??
         assert user.user_profile(12345, 2) #technically u_id can be anything but 1
