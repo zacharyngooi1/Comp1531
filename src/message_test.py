48b77591cc1acbf9b48429fd9f7069faab384e2f
Mufeed <z5120474@student.unsw.edu.au>
2020-03-08 15:16:03 +1100

    final changes

--- src/message_test.py
+++ src/message_test.py
@@ -7,27 +7,76 @@ from auth import auth_register
 
 #Assumptions
 #All the inputs are valid
+def user_register(email, password, name_first, name_last):
+	tmp = auth_register(email, password, name_first, name_last)    
+	return {
+        'u_id': tmp['u_id'],
+        'token': tmp['token'],
+		'name_first': name_first,
+		'name_last': name_last, 
+		'email': email,
+		'password': password
+    }
+
+#Function to create a new channel. Returns information about that channel 
+def new_channel(token, name, is_public): 
+	tmp = channels_create(token, name, is_public)
+	return { 
+		'channel_id': tmp['channel_id'],
+		'token': token,
+		'name': name,
+		'is_public': is_public
+	}
 
 #Checks weather the message has been sent
 def test_message_send():
-     #by default message_id is 1.
-    assert message.message_send(12345, 1, 'Hello world') == 1  
+    #by default message_id is 1.
+    user_results = user_register('email@email.com', 'Password', 'Citzen', 'Good')
+    results = new_channel(user_results['token'], 'Test', True)
+
+    #send a message
     message_id = message.message_send(12345, 1, 'Hello world')
     #check if the message has actually been sent
-    assert channel.channel_messages['messages']['message_id'] == 1  
+    message_log = channel.channel_messages(12345,1,0)
+    flag =0
+    for i in message_log['messages']:
+        if i['message_id'] ==  message_id:
+            flag = 1 
+    assert flag == 1  
     
 #Checks weather the message has been removed
 def test_message_remove():
-    out = message.message_send(12345, 1, 'hello hru')
-    assert message.message_remove(12345,1) == {}
+    #by default message_id is 1.
+    user_results = user_register('email@email.com', 'Password', 'Citzen', 'Good')
+    results = new_channel(user_results['token'], 'Test', True)
+
+    #send on channel 1
+    message_id = message.message_send(12345, 1, 'Hello world')
+    #check if the function outputs the correct output
+    assert message.message_remove(12345,message_id) == {}
+    message_log = channel.channel_messages(12345,1,0)
+    flag =0
+    for i in message_log['messages']:
+        if i['message_id'] ==  message_id:
+            flag = 1 
+    assert flag ==0
 
 #Checks weather the message has been edited
 def test_message_edit():
-    out = message.message_send(12345, 1, 'Bye World')
+    user_results = user_register('email@email.com', 'Password', 'Citzen', 'Good')
+    results = new_channel(user_results['token'], 'Test', True)
+
+    #send on channel 1
+    message_id = message.message_send(12345, 1, 'Bye world')
+
     message_id = message.message_edit(12345, 1, 'Hello world')
     #check if the message has been edit
-    assert channel.channel_messages['messages']['message_id'] == "Hello world"  
-
+    message_log = channel.channel_messages(12345,1,0)
+    for i in message_log['messages']:
+        if i['message_id'] ==  message_id and i['message'] == "Hello world":
+            flag = 1 
+    assert flag == 1
+    
 def test_message_except():
     #an example of when there would be an inout error ie. message longer than 1000
     with pytest.raises(InputError) as e:
