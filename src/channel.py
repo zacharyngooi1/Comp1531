4034c5d5ea765cbead1708f60a2e711a4f37041a
Devansh Kala <z5161391@ad.unsw.edu.au>
2020-03-26 22:39:07 +1100

    Some unneded changes

--- src/channel.py
+++ src/channel.py
@@ -1,8 +1,31 @@
+data = {
+    #'user' is a list of dictionaries of users
+    'user': [] 
+}
+
 def channel_invite(token, channel_id, u_id):
+    #@Muffeed you should go back to auth_register and add another key in the dictionary called channel_ids
+    #It will be something like return_dict['channel_ids'] = []
+    for user in data['user']:
+        if user['token'] == token:
+            if user['u_id'] == u_id:
+                user['channel_ids'].append(channel_id)
     return {
     }
 
 def channel_details(token, channel_id):
+    all_members = [] # A list with dictionaries of users
+    owner_members = [] # A list with dictionaries of users
+    user_info_dictionary = {}
+    for user in data['user']:
+        for a_channel_id in user['channel_ids']:
+            if a_channel_id == channel_id:
+                all_members.append({
+                        'u_id': user["u_id"], 
+                        'name_first': user["name_first"], 
+                        'name_last': user["name_last"],
+                    })
+            
     return {
         'name': 'Hayden',
         'owner_members': [
--- src/message.py
+++ src/message.py
@@ -1,6 +1,54 @@
+from flask import Flask, request 
+from json import dumps
+
+APP = Flask(__name__)
+
+data = {
+    'user': []
+}
+
 def message_send(token, channel_id, message):
+    
     return {
         'message_id': 1,
+    }
+
+def message_remove(token, message_id):
+    return {
+    }
+
+def message_edit(token, message_id, message):
+    return {
+    }
+
+def message_react(token, message_id, react_id):
+    return {
+    }
+
+
+#mesage_unreact returns en empty dictionary
+#Given a message within a channel the authorised user is part of, remove a "react" to that particular message
+def message_unreact(token, message_id, react_id):
+    #Checking if the input is valid
+    if token is None:
+        raise InputError
+    if message_id is None:
+        raise InputError
+    if react_id is None:
+        raise InputError
+    auth_register()
+    auth_login()
+    react_id = 0
+    return {}
+
+#message_pin returns an empty dictionary
+#Given a message within a channel, mark it as "pinned" to be given special display treatment by the frontend
+def message_pin(token, message_id):
+    return {
+    }
+
+def message_unpin(token, message_id):
+    return {
     }
 
 def message_remove(token, message_id):
