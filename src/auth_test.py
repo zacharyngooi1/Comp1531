e687cc76a77482527f8086578bc583fa8732f9e2
Zachary Ngooi <z5228846@ad.unsw.edu.au>
2020-03-28 00:41:29 +1100

    tests done

--- src/auth_test.py
+++ src/auth_test.py
@@ -1,4 +1,18 @@
-from auth import auth_register, auth_login, auth_logout
-import pytest
-from error import InputError
+import pytest 
+from channel import channel_join, channel_addowner, channel_removeowner, channel_details
+from channel import channel_list, channels_list_all, channels_create, channel_leave, channel_invite
+from auth import auth_register
+from error import InputError, AccessError
+from user import user_profile_setname, user_profile_setemail, user_profile_sethandle, user_profile
+from other import users_all, search
+import datetime
+from auth import auth_register
+from db import token_check, email_check, email_dupe_check, get_messages_store, channel_check, u_id_check, handle_check, get_channel_store
+
+def test_auth_register_invalid():
+	with pytest.raises(InputError):
+		auth_register('email@hat', 'password','name_first', 'name_last')
+	
+
+
 
